9bd684c975981527b4398baf76f0a679f1adb3dd
mxxt1 <matt@dustcloudlabs.com>
2019-11-07 18:18:17 -0500

    db design, migration built, migration deployed

--- data/migrations/20191107145106_create_recipes_db.js
+++ data/migrations/20191107145106_create_recipes_db.js
@@ -1,6 +1,54 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('recipes', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 255).notNullable();
+  })
+
+  .createTable('ingredients', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 255).notNullable();
+
+      tbl.float('quantity').notNullable();
+
+    })
+
+    .createTable('instructions', tbl => {
+        tbl.increments();
+
+        tbl.string('instruction', 255).notNullable();
+
+        tbl.integer('step').unsigned().notNullable();
+
+        tbl.integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipe')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+
+    .createTable('recipe_details', tbl => {
+
+        tbl.increments();
+
+        tbl.integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipe')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+
+        tbl.integer('ingredient_id')
+        .unsigned()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
 };
 
 exports.down = function(knex) {
